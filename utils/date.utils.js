6a55d65239c630e94377a267d676694b05720bc7
AndyNyRindra <92746662+AndyNyRindra@users.noreply.github.com>
2024-02-25 18:33:53 +0300

    rappel rdv

--- utils/date.utils.js
+++ utils/date.utils.js
@@ -1,3 +1,14 @@
 exports.toLocale = (date) => {
     return new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds()));
 }
+
+exports.formatDate = (date) => {
+    const d = new Date(date);
+    const day = String(d.getUTCDate()).padStart(2, '0');
+    const month = String(d.getUTCMonth() + 1).padStart(2, '0'); // Les mois sont de 0 à 11, donc nous ajoutons 1
+    const year = d.getUTCFullYear();
+    const hours = String(d.getUTCHours()).padStart(2, '0');
+    const minutes = String(d.getUTCMinutes()).padStart(2, '0');
+
+    return `${day}/${month}/${year} à ${hours}:${minutes}`;
+}
